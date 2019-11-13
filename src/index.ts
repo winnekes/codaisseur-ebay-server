48e19f5e1957ccc2b312419f4f2a77d89f19a609
Simona Winnekes <simona.winnekes@googlemail.com>
2019-11-13 11:30:04 +0100

    Implemenent auth (not using it yet in routers

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 // src/index.ts
 import 'reflect-metadata';
-import { createKoaServer } from 'routing-controllers';
+import { createKoaServer, Action } from 'routing-controllers';
 import * as cors from '@koa/cors';
 
 import setupDb from './db';
 import AdvertisementController from './advertisements/controller';
+import { verify } from './jwt';
 
 const port = process.env.PORT || 4000;
 
 const app = createKoaServer({
     controllers: [AdvertisementController],
+    authorizationChecker: (action: Action) => {
+        const header: string = action.request.headers.authorization;
+
+        if (header && header.startsWith('Bearer ')) {
+            const [, token] = header.split(' ');
+            return !!(token && verify(token));
+        }
+        // ...
+        return false;
+    },
 });
 app.use(cors());
 
